88d51aa5eedc9f870b100f4b909a6a7045fe90b5
Samy-in <rojoldashec34@gmail.com>
2024-12-14 22:52:09 +0530

    Input validations using Middlewares and Global catches used for errors

--- 4_MWexamples2.js
+++ 4_MWexamples2.js
@@ -1,5 +1,7 @@
 // this is input validation examples in express
 
+// Why do we need input validation becoz if ann user get your websites with any inputs then it not show some functional
+// errors in which the name of librarys are showing soinstead of that use show them an error msg.
 const express = require("express");
 const app = express();
 
@@ -12,10 +14,16 @@ app.post("/health-checkup" , function (req,res){
 
     res.send("You have " + kidneylength + " kidneys");
 });
+// Here, this will tell us how many no.of.kidneys are there but if user instead of writting no.of.array it has
+// given a gibbreish or characters or any other than inputs then we use global catches for errors. 
+
+// ##### Global catch is another type of middleware which takes 4 parameters/inputs which is written 
+// After All the routings are mentioned.######
+
 // now we use global catches which is used for giving a user better error message where it has four parameters
 app.use((err, req, res, next) => {
     res.status(500).send('An internal server error occured')
 });
 
-
 app.listen(3003);
+
