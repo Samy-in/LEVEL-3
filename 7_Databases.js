feac3b3ca13bd5a0d1fcd1c8cad27d6a94f0605c
Samy-in <rojoldashec34@gmail.com>
2024-12-22 01:21:11 +0530

    All about different databases, Mongoose to mongoDB connection

--- 7_Databases.js
+++ 7_Databases.js
@@ -1,7 +1,7 @@
 // In real world, User hits requests to the backend then backend hits to database , User doesnot have access to this database.
 
 // Types of databases 
-    // GrapghDB = used for specific niche purposes.
+    // GraphDB = used for specific niche purposes.
     // VectorDB = Its popular for machine learning algorithms
     // SQL-DB = Popular for more structured data operations where we store data in structured manner
     // Ususally it more used in full stack applications and used in open sources code
@@ -12,7 +12,7 @@
     // 3) Its schemaless (VVVERY IMP) = means no structure is required to store this json data in database.
 
 // How to start ? = 1) Download mongodb
-    //2) Get your mongodb connections
+    // 2) Get your mongodb connections
     // 3) Download mongoDB compass and go explore.
 
 // Now that we have connected mongodb compass with making database by making the link or json format.
@@ -63,84 +63,86 @@ app.post("/Sign-up" , async function (req , res){
 app.listen(3000);
 
 
-// const express = require("express");
-// const jwt = require("jsonwedtoken");
-// const mongoose = require("mongoose");
-// const jwtPassword = "1123456";
+const express = require("express");
+const jwt = require("jsonwedtoken");
+const mongoose = require("mongoose");
+const jwtPassword = "1123456";
 
-// mongoose.connect(
-//     ""
-// );
+mongoose.connect(
+    ""
+);
 
-// const user = mongoose.model("Users", {
-//     name: String,
-//     Username:String,
-//     password: String,
-// });
+const user = mongoose.model("Users", {
+    name: String,
+    Username:String,
+    password: String,
+});
 
-// const app = express();
-// app.use(express.json());
-// const All_Users = [
-//     {
-//         Username: "AlfansoDavid@gmail.com",
-//         Password: "123",
-//         Name: "Alfanso David"
-//     },
-//     {
-//         Username: "RobertoCarlos@gmail.com",
-//         Password: "123443",
-//         Name: "Roberto Carlos"
-//     },
-//     {
-//         Username: "PeterCrouch@gmail.com",
-//         Password: "3452",
-//         Name: "Peter Crouch"
-//     },
-// ];
+const app = express();
+app.use(express.json());
+const All_Users = [
+    {
+        Username: "AlfansoDavid@gmail.com",
+        Password: "123",
+        Name: "Alfanso David"
+    },
+    {
+        Username: "RobertoCarlos@gmail.com",
+        Password: "123443",
+        Name: "Roberto Carlos"
+    },
+    {
+        Username: "PeterCrouch@gmail.com",
+        Password: "3452",
+        Name: "Peter Crouch"
+    },
+];
 
-// function UserExists(username, password) {
-//     // should checks in databases
-// }
+function UserExists(username, password) {
+    // should checks in databases
+}
 
-// app.post("/sign-in", function(req , res){
-//     const username = req.body.username;
-//     const password = req.body.password;
+app.post("/sign-in", function(req , res){
+    const username = req.body.username;
+    const password = req.body.password;
 
-//     if(!UserExists(username , password)){
-//         return res.status(403).json({
-//             msg: "User doesn't exist in our db"
-//         });
-//     }
-//     var token = jwt.sign({username : "username"}, jwtPassword);
-//     return res.json({
-//         token,
-//     });
-//     // on above there is a token made which takes json as input with some password which is practically wrong becoz original
-//     // password is jwtPassword , so it takes input as json and converts into a long string and return it to the user.
-// });
+    if(!UserExists(username , password)){
+        return res.status(403).json({
+            msg: "User doesn't exist in our db"
+        });
+    }
+    var token = jwt.sign({username : "username"}, jwtPassword);
+    return res.json({
+        token,
+    });
+    // on above there is a token made which takes json as input with some password which is practically wrong becoz original
+    // password is jwtPassword , so it takes input as json and converts into a long string and return it to the user.
+});
 
-// app.get("/users", function (req, res) {
-//     const token = req.headers.authorization;
-
-//     try {
-//         const decoded = jwt.verify(token, jwtPassword);
-//         const username = decoded.username;
-//         // return a list of users other than this username
-//     } catch (err) {
-//         return res.status(403).json({
-//             msg: "Invalid token",
-//         });
-//     }
+app.get("/users", function (req, res) {
+    const token = req.headers.authorization;
+// this is try-catch syntax 
+// why we use this? = If we don't know that the statement that is written is wrong or faulty then we throw that inside try{}
+//  And if error exists then it will stop the control and print whatever inside the catch{} is.
+    try {
+        const decoded = jwt.verify(token, jwtPassword);  // here we are checking if the jwtpassword is right 
+        const username = decoded.username;   // If the password is right then give the username of the user and if NO goto Catch()
+        // return a list of users other than this username
+    } catch (err) {
+        return res.status(403).json({
+            msg: "Invalid token",
+        });
+    }
     
-//     res.json({
-//         users: All_Users.filter(function(values){
-//             if(values.username == username){
-//                 return false;
-//             } else{  // this code returns all other user's data rather than his own data.
-//                 return true;
-//             }
-//         })
-//     })
-// });
+    res.json({
+        users: All_Users.filter(function(values){
+            if(values.username == username){
+                return false;
+            } else{  // this code returns all other user's data rather than his own data.
+                return true;
+            }
+        })
+    })
+});
 
-// app.listen(3000);
+app.listen(3000);
